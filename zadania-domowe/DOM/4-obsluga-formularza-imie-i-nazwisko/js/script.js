450b12afa857f0c376893a2f9019b9508af9ed6e
ppopow <pawel.popow@gmail.com>
2020-11-16 11:53:59 +0100

    nowezadanie4

---
+++ zadania-domowe/DOM/4-obsluga-formularza-imie-i-nazwisko/js/script.js
@@ -0,0 +1,16 @@
+console.log(`WARSZTAT - Obsługa formularza 'Imię i nazwisko'`);
+
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    console.log(`Imię: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form');
+
+
+form.addEventListener('submit', submitForm);
